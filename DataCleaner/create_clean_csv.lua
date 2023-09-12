9c281a46f9c46fe702915af801752983957c1b46
dowdyj <joeldowdy12@gmail.com>
2023-09-12 03:58:11 -0700

    Create basic data cleaning script

--- DataCleaner/create_clean_csv.lua
+++ DataCleaner/create_clean_csv.lua
@@ -4,8 +4,20 @@
 -- PREVIOUS_KEY_CODE, KEY_CODE, IS_OVERLAPPING_WITH_PREVIOUS, DELAY_TO_PRESS, TIME_HELD
 -- 56, 31, 1, 300, 40
 
-local function get_txt_file_names_in_current_directory()
-    local output = io.popen("ls -a")
+local data_keys = {}
+data_keys.PARTICIPANT_ID = 1
+data_keys.TEST_SECTION_ID = 2
+data_keys.SENTENCE = 3
+data_keys.USER_INPUT = 4
+data_keys.KEYSTROKE_ID = 5
+data_keys.PRESS_TIME = 6
+data_keys.RELEASE_TIME = 7
+data_keys.LETTER = 8
+data_keys.KEYCODE = 9
+
+
+local function get_txt_file_names_in_raw_directory()
+    local output = io.popen("ls -a ../Data/raw/")
 
     if output == nil then return end
 
@@ -23,8 +35,8 @@ local function get_txt_file_names_in_current_directory()
 end
 
 local function create_result_file()
-    io.popen("touch result.csv")
-    local result_file = io.open("result.csv", "w+")
+    io.popen("touch ../Data/processed/result.csv")
+    local result_file = io.open("../Data/processed/result.csv", "w+")
     
     if result_file == nil then
         return nil
@@ -54,45 +66,103 @@ local function split(input_string, delimeter)
     return results
 end
 
-local filenames = get_txt_file_names_in_current_directory()
-if filenames == nil then print("Failed to get filenames"); return end
-
-local result_file = create_result_file()
-if result_file == nil then print("Failed to create result file"); return end
-
-
-for _, filename in pairs(filenames) do
+local function get_file_lines(filename)
     local file_lines = {}
 
     local data_file = io.open(filename, "r")
-    if data_file == nil then print("Failed to open data file with name " .. filename); return end
-
-    for index, line in data_file:lines() do
+    if data_file == nil then return nil end
+    for line in data_file:lines() do
         table.insert(file_lines, line)
     end
 
     data_file:close()
 
-    -- Create data entry for clean CSV
 
-    local previous_character, current_character, is_overlapping, time_held, time_since_key_press
-    for index, line in ipairs(file_lines) do
-        if index == 1 then
-            previous_character = " "
-        else
-            previous_character = file_lines[index - 1][]
-        end
+    return file_lines
+end
+
+
+---comment
+---@param file_handle file*
+---@param results table
+local function write_participants_result_to_file(file_handle, results)
+    
+    -- {current_character_code, previous_character_code, time_held, time_since_key_press, is_overlapping}
+    local key_delay_total, key_count, key_delay_avg = 0, 0, 0
+    for _, row in ipairs(results) do
+        key_delay_total = key_delay_total + row[4]
+        key_count = key_count + 1
     end
 
-    result_file:write(filename)
+    key_delay_avg = math.floor(key_delay_total / key_count)
+    for _, row in ipairs(results) do
+        
+        file_handle:write(row[1] .. "," .. row[2] .. "," .. row[3] .. "," .. row[4] .. "," .. row[5] .. "," .. key_delay_avg .. "\n")
+    end
 
 end
 
-result_file:close()
 
-split("1002	10319	The results were expected back Tuesday.	The results were expected back Tuesday.	494136	1471948496904	1471948497169	SHIFT	16", "\t")
 
--- For every file in folder ending in txt
--- -- for each line in file
--- -- tokenize on TAB
--- -- Combine last two time columns for time elapsed
+
+local function main()
+    local filenames = get_txt_file_names_in_raw_directory()
+    if filenames == nil then print("Failed to get filenames"); return end
+    
+    local result_file = create_result_file()
+    if result_file == nil then print("Failed to create result file"); return end
+    
+    
+    for _, filename in pairs(filenames) do repeat
+        local file_lines = get_file_lines("../Data/raw/" .. filename)
+        if file_lines == nil then print("Failed to load file with filename: " .. filename); break end
+    
+    
+        -- Create data entry for clean CSV
+        local previous_character_code, current_character_code, is_overlapping, time_held, time_since_key_press
+        local previous_line, current_line
+    
+        local participants_results = {}
+    
+        for index, line in ipairs(file_lines) do repeat
+            if index == 1 then
+                break    
+            end
+
+            current_line = split(line, "\t")
+    
+            if previous_line ~= nil and current_line[data_keys.TEST_SECTION_ID] ~= previous_line[data_keys.TEST_SECTION_ID] then
+                previous_line = nil
+                write_participants_result_to_file(result_file, participants_results)
+                participants_results = {}
+            end
+    
+            if previous_line ==  nil then
+                previous_character_code = "0"
+                is_overlapping = "0"
+                time_since_key_press = "-1"
+            else
+                previous_character_code = previous_line[data_keys.KEYCODE]
+                is_overlapping = tonumber(previous_line[data_keys.RELEASE_TIME]) > tonumber(current_line[data_keys.PRESS_TIME]) and "1" or "0"
+                time_since_key_press = tonumber(current_line[data_keys.PRESS_TIME]) - tonumber(previous_line[data_keys.PRESS_TIME])
+            end
+    
+            current_character_code = current_line[data_keys.KEYCODE]
+            time_held = tonumber(current_line[data_keys.RELEASE_TIME]) - tonumber(current_line[data_keys.PRESS_TIME])
+    
+            table.insert(participants_results, {current_character_code, previous_character_code, time_held, time_since_key_press, is_overlapping})
+    
+            previous_line = current_line
+        until true end
+    
+        write_participants_result_to_file(result_file, participants_results)
+    
+    until true end
+    
+    result_file:close()
+    
+
+end
+
+
+main()
