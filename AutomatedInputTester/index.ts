16aa5b119dab34af0482b5d53d66fe4228f39b2d
dowdyj <joeldowdy12@gmail.com>
2023-09-17 04:13:33 -0700

    Update automated tests to look more pleasing

--- AutomatedInputTester/index.ts
+++ AutomatedInputTester/index.ts
@@ -9,31 +9,35 @@ function sleep(ms: number) {
 
 (async () => {
     const TIME_TO_WAIT = 3000;
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [`--start-maximized`],
+        defaultViewport: null
+    });
 
-    let textToType = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    
     try {
         const page = await browser.newPage();
-        await page.setViewport({width: 1080, height: 1024});
-    
+        
         await page.goto('http://localhost:8080/');
-    
+        await sleep(TIME_TO_WAIT);
+
+        await ((await page.$("#results-area")) as ElementHandle<Element>).scrollIntoView();
         await page.waitForSelector("#textbox");
         await sleep(TIME_TO_WAIT);
-        await page.type('#textbox', textToType);
+        await page.type('#textbox', "This text is an example of typical instant typing used by bots.");
         await sleep(TIME_TO_WAIT);
         await page.click("#submit-data");
         
         await sleep(TIME_TO_WAIT);
-        await page.type('#textbox', textToType, { delay: 100 });
+        await page.type('#textbox', "This text has a constant delay, a built in feature of the automation software.", { delay: 100 });
         await sleep(TIME_TO_WAIT);
         await page.click("#submit-data");
         await sleep(TIME_TO_WAIT);
 
         let base = 100
         let variation = 30
-        for (const char of textToType) {
+        let textToType1 = "This text uses a more sophisticated method which adds random variation to the input.";
+        for (const char of textToType1) {
             await page.type('#textbox', char);
             await sleep(base + Math.random() * variation)
         }
@@ -41,17 +45,16 @@ function sleep(ms: number) {
         await page.click("#submit-data");
         await sleep(TIME_TO_WAIT);
 
+        let textToType2 = "This text also adds random variation to the input, but this time even more variation.";
         base = 100
         variation = 70
-        for (const char of textToType) {
+        for (const char of textToType2) {
             await page.type('#textbox', char);
             await sleep(base + Math.random() * variation)
         }
         await sleep(TIME_TO_WAIT);
         await page.click("#submit-data");
         await sleep(TIME_TO_WAIT);
-
-        await sleep(TIME_TO_WAIT * 10000);
     } 
     catch (error) {
         console.log(error);    
