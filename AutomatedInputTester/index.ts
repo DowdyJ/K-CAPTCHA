66805db201ae94ac80c80b959ba16eae093b5356
dowdyj <joeldowdy12@gmail.com>
2023-09-16 21:05:43 -0700

    Update automated tests to be more rigorous

--- AutomatedInputTester/index.ts
+++ AutomatedInputTester/index.ts
@@ -11,6 +11,8 @@ function sleep(ms: number) {
     const TIME_TO_WAIT = 3000;
     const browser = await puppeteer.launch({headless: false});
 
+    let textToType = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    
     try {
         const page = await browser.newPage();
         await page.setViewport({width: 1080, height: 1024});
@@ -19,18 +21,37 @@ function sleep(ms: number) {
     
         await page.waitForSelector("#textbox");
         await sleep(TIME_TO_WAIT);
-        await page.type('#textbox', 'This text is typed instantly.');
+        await page.type('#textbox', textToType);
         await sleep(TIME_TO_WAIT);
-        await page.waitForSelector("#submit-data");
         await page.click("#submit-data");
         
         await sleep(TIME_TO_WAIT);
-        await page.type('#textbox', 'This text is typed slower using pupeteer\'s feature to do so.', { delay: 100 });
+        await page.type('#textbox', textToType, { delay: 100 });
         await sleep(TIME_TO_WAIT);
-        await page.waitForSelector("#submit-data");
         await page.click("#submit-data");
         await sleep(TIME_TO_WAIT);
 
+        let base = 100
+        let variation = 30
+        for (const char of textToType) {
+            await page.type('#textbox', char);
+            await sleep(base + Math.random() * variation)
+        }
+        await sleep(TIME_TO_WAIT);
+        await page.click("#submit-data");
+        await sleep(TIME_TO_WAIT);
+
+        base = 100
+        variation = 70
+        for (const char of textToType) {
+            await page.type('#textbox', char);
+            await sleep(base + Math.random() * variation)
+        }
+        await sleep(TIME_TO_WAIT);
+        await page.click("#submit-data");
+        await sleep(TIME_TO_WAIT);
+
+        await sleep(TIME_TO_WAIT * 10000);
     } 
     catch (error) {
         console.log(error);    
