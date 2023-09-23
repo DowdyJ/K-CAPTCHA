3e54f8f32f7c7278e8e0aafb3545f51231bc39b0
dowdyj <joeldowdy12@gmail.com>
2023-09-23 00:38:09 -0700

    Update README Update gitignore Remove unnecessary scripts

--- build_docker_images.sh
+++
@@ -1,2 +0,0 @@
-docker build -t kbcaptcha-backend KeyInputTester/
-docker build -t kbcaptcha-frontend-demo Frontend/kbcaptcha-demo/
--- run_solution.sh
+++
@@ -1,3 +0,0 @@
-docker network create --driver=bridge kbcaptcha-network
-docker run -d -p 5000:5000 --network=kbcaptcha-network --name=kbcaptcha-python-backend kbcaptcha-backend
-docker run -d -p 8080:8080 --network=kbcaptcha-network --name=kbcaptcha-java-frontend kbcaptcha-frontend-demo
